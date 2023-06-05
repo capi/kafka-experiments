3bb5432e7198255f0f0ddafe1507db12ce05433c
Martin Carpella <martin.carpella@gmx.net>
2023-06-05 21:16:00 +0200

    Initial project structure

---
+++ src/main/java/cc/dont_panic/experiments/kafka/KafkaExperimentStarter.java
@@ -0,0 +1,7 @@
+package cc.dont_panic.experiments.kafka;
+
+public class KafkaExperimentStarter {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
