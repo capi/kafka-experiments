c24be4d55eb739f5ac35f68ff960be198021b84a
Martin Carpella <martin.carpella@gmx.net>
2023-06-05 22:10:11 +0200

    Remove unused static variable

--- src/main/java/cc/dont_panic/experiments/kafka/KafkaExperimentStarter.java
+++ src/main/java/cc/dont_panic/experiments/kafka/KafkaExperimentStarter.java
@@ -4,8 +4,6 @@ import java.util.concurrent.ExecutionException;
 
 public class KafkaExperimentStarter {
 
-    private static final String STATE_TOPIC = "state-topic";
-
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         KafkaConfig config = new KafkaConfig();
         KafkaTopicCreator kafkaTopicCreator = new KafkaTopicCreator(config);
